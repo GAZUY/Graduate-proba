287c1cad0c73240d0a60764f5de550d98973ee00
GAZUY <gazuyd@gmail.com>
2024-03-09 14:18:10 +0300

    [piojk]

--- src/main.ts
+++ src/main.ts
@@ -29,42 +29,79 @@ let x = 0,
     y = 0;
 
 document.addEventListener('keydown', function(e){
-  if(e.code == 'ArrowLeft') y -= 5
-  if(e.code == 'ArrowRight') y += 5
-  if(e.code == 'ArrowUp') x += 5
-  if(e.code == 'ArrowDown') x -= 5
+  if(e.code == 'ArrowLeft') y -= 3
+  if(e.code == 'ArrowRight') y += 3
+  if(e.code == 'ArrowUp') x += 3
+  if(e.code == 'ArrowDown') x -= 3
  
  cube.style.transform = `rotateY(${y}deg) rotateX(${x}deg)`;
 });
 
 document.addEventListener('DOMContentLoaded',function(){
  
-  cube.dataset.x = '0'
-  cube.dataset.y = '0'
-  
+  let x = 0
+  let y = 0
   document.onmousedown = function(event){
-    let x = 0
-    let y = 0
     cube.classList.remove('rotate')
     let target = event;
     let disX = target.clientX - y
     let disY = target.clientY - x
-    document.onmousemove = function(event){
-      let target = event;
-      x = target.clientX - disX
-      y = target.clientY - disY
-      cube.style.transform = '  rotateY('+cube.dataset.x+x+'deg) rotateX('+(+cube.dataset.y-y)+'deg)'
-    };
-    document.onmouseup = function(){
-      document.onmousemove = null
-      document.onmouseup = null
-      cube.dataset.x = x.toString()
-      cube.dataset.y = y.toString()
-      
+      document.onmousemove = function(event){
+        let target = event;
+        x = target.clientX - disX
+        y = target.clientY - disY
+        cube.style.transform = '  rotateY('+x+'deg) rotateX('+(-y)+'deg)'
+      };
+      document.onmouseup = function(){
+          document.onmousemove = null
+          document.onmouseup = null
       };
       return false
   }
 },false)
+// let cube = document.querySelector('.cube') as HTMLElement
+// cube.classList.add('rotate')
+
+// let x = 0,
+//     y = 0;
+
+// document.addEventListener('keydown', function(e){
+//   if(e.code == 'ArrowLeft') y -= 5
+//   if(e.code == 'ArrowRight') y += 5
+//   if(e.code == 'ArrowUp') x += 5
+//   if(e.code == 'ArrowDown') x -= 5
+ 
+//  cube.style.transform = `rotateY(${y}deg) rotateX(${x}deg)`;
+// });
+
+// document.addEventListener('DOMContentLoaded',function(){
+ 
+//   cube.dataset.x = '0'
+//   cube.dataset.y = '0'
+  
+//   document.onmousedown = function(event){
+//     let x = 0
+//     let y = 0
+//     cube.classList.remove('rotate')
+//     let target = event;
+//     let disX = target.clientX - y
+//     let disY = target.clientY - x
+//     document.onmousemove = function(event){
+//       let target = event;
+//       x = target.clientX - disX
+//       y = target.clientY - disY
+//       cube.style.transform = '  rotateY('+cube.dataset.x+x+'deg) rotateX('+(+cube.dataset.y-y)+'deg)'
+//     };
+//     document.onmouseup = function(){
+//       document.onmousemove = null
+//       document.onmouseup = null
+//       cube.dataset.x = x.toString()
+//       cube.dataset.y = y.toString()
+      
+//       };
+//       return false
+//   }
+// },false)
 
 //========================================================================================
 
